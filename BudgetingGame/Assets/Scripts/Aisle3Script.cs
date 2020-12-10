1c34736443fa0c990682e034de1a88e72332a1d8
Darshna Doshy <darshna@Darshnas-MacBook-Pro.local>
2020-12-10 15:23:18 -0600

    Scripts created

---
+++ BudgetingGame/Assets/Scripts/Aisle3Script.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Aisle3Script : MonoBehaviour
+{
+    public float total = 0.0f;
+    public void Button1(float value)
+    {
+        total += value;
+    }
+
+    public void Button2(float value)
+    {
+        total += value;
+    }
+
+    public void Button3(float value)
+    {
+        total += value;
+    }
+
+    public void Button4(float value)
+    {
+        total += value;
+    }
+
+    public void Button5(float value)
+    {
+        total += value;
+    }
+
+    public void Button6(float value)
+    {
+        total += value;
+    }
+
+    public float GetAisle3Total()
+    {
+        return total;
+    }
+}
